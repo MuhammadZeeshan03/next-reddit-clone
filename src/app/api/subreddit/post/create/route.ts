8b3d29b024648e9b2d4277768f35591eab36de54
Muhammad Zeesahan <mzeeshan.bscs19seecs@seecs.edu.pk>
2023-11-13 15:02:45 +0500

    creation and submission of a post

---
+++ src/app/api/subreddit/post/create/route.ts
@@ -0,0 +1,45 @@
+import { getAuthSession } from "@/lib/auth";
+import { z } from "zod";
+import { db } from "@/lib/db";
+import { subredditSubscriptionValidator } from "@/lib/validators/subreddit";
+import { postValidator } from "@/lib/validators/post";
+export async function POST(req: Request) {
+    try {
+        const session = await getAuthSession();
+        if (!session?.user) {
+            return new Response("Unauthorized", { status: 401 });
+        }
+        const body = await req.json();
+
+        const { subredditId, title, content } = postValidator.parse(body);
+
+        const subscriptionExists = await db.subscription.findFirst({
+            where: {
+                subredditId,
+                userId: session.user.id,
+            },
+        });
+        if (!subscriptionExists) {
+            return new Response("Subscribe to Post", { status: 400 });
+        }
+
+        await db.post.create({
+            data: {
+                title,
+                content,
+                authorId: session.user.id,
+                subredditId,
+
+            },
+        });
+
+        return new Response('OK');
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            return new Response("Invalid Request data Passed", { status: 422 });
+        }
+        return new Response("Could not post to subreddit at this time, Please try again later", {
+            status: 500,
+        });
+    }
+}
--- src/components/ui/Editor.tsx
+++ src/components/ui/Editor.tsx
@@ -6,7 +6,12 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { FC, useCallback, useRef, useState, useEffect } from 'react'
 import type EditorJS from '@editorjs/editorjs'
 import { uploadFiles } from '@/lib/uploadthing'
-
+import { toast } from '@/hooks/use-toast'
+import { useMutation } from "@tanstack/react-query";
+import axios from 'axios'
+import { usePathname } from 'next/navigation'
+import { Router } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 interface EditorProps {
   subredditId: string
@@ -27,14 +32,11 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
   })
 
   const ref = useRef<EditorJS>()
-
   const [isMounted, setIsMounted] = useState<boolean>(false)
+  const _titleRef = useRef<HTMLTextAreaElement>(null)
+  const pathname = usePathname()
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsMounted(true)
-    }
-  }, [])
+  const router = useRouter()
 
   const initializeEditor = useCallback(async () => {
 
@@ -98,36 +100,113 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
 
 
   }, [])
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+
+      for (const [_key, value] of Object.entries(errors)) {
+        toast({
+          title: 'Something went wrong',
+          description: (value as { message: string }).message,
+          variant: 'destructive'
+        })
+      }
+    }
+  }, [errors])
 
   useEffect(() => {
 
     const init = async () => {
       await initializeEditor()
       setTimeout(() => {
-        //set focus to title
-      })
+        _titleRef.current?.focus()
+      }, 0)
     }
     if (isMounted) {
       init()
-      return () => { }
+      return () => {
+        ref.current?.destroy()
+        ref.current = undefined
+      }
+
 
     }
 
   }, [isMounted, initializeEditor])
 
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({ title, content, subredditId }: PostCreationRequest) => {
+
+      const payload: PostCreationRequest = {
+        subredditId,
+        title,
+        content,
+      }
+      const { data } = await axios.post('/api/subreddit/post/create', payload)
+      return data
+
+    },
+    onError: () => {
+      return toast({
+        title: 'Something went wrong',
+        description: 'Your post was not published. Please try again',
+        variant: 'destructive'
+      })
+    },
+    onSuccess: () => {
+      // r/community/submit into e/mycommunity 
+      const newPathName = pathname.split('/').slice(0, -1).join('/')
+      router.push(newPathName)
+
+      router.refresh()
+      return toast({
+        description: 'Your post was published',
+      })
+    }
+  })
+
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save()
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId
+    }
+
+    createPost(payload)
+  }
+
+  if (!isMounted) return null
+
+  const { ref: titleRef, ...rest } = register('title')
 
   return (
     <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
       <form id='subreddit-post-form'
-        className='w-fit' onSubmit={() => { }}>
+        className='w-fit' onSubmit={handleSubmit(onSubmit)}>
         <div className='prose prose-stone dark:prose-invert'>
           <TextareaAutosize placeholder='Title'
+            {...rest}
+            {...register('title')}
+            ref={((e) => {
+              titleRef(e)
+
+              // @ts-ignore
+              _titleRef.current = e
+            })}
             className='w-full resize-none appearance-none overflow-hidden bg-trasparent text-5xl font-bold focus:outline-none' />
 
           <div id='editor' className='min-h-[500px]' />
         </div>
       </form>
-    </div>
+    </div >
   )
 }
 
